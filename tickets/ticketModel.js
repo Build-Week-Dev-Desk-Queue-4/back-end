a4d49be0823365784591d12d43c97c3d56d2d982
Vincent Sanders <vincesanders@hotmail.com>
2020-02-28 13:24:02 -0600

    Added ticket model amd ticket router.

---
+++ tickets/ticketModel.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    get,
+    getAllOpen,
+    getById,
+    getBy,
+    getNewest,
+    getOldest,
+    getComments
+}
+
+//STRETCH: display number of comments with tickets.
+function get() {
+    return db('tickets');
+}
+
+function getById(id) {
+    return db('tickets').where({ id }).first();
+}
+
+//this should allow get by category, title, description, resolved or not, not being solved
+function getBy(filter) {
+    return db('tickets').where(filter);
+}
+
+//return all tickets that aren't resolved AND aren't being solved.
+function getAllOpen() {
+    return db('tickets').where({resolved: false, being_solved: false});
+}
+
+function getNewest() {
+    return db('tickets').orderBy('created_at');
+}
+
+function getOldest() {
+    return db('tickets').orderBy('created_at', 'desc');
+}
+
+function getComments(id) {
+    return db('comments as c')
+        .join('users as u', 'u.id', 'c.commenter_id')
+        .select('c.id', 'u.role', 'u.username', 'u.first_name', 'u.last_name', 'c.comment', 'c.is_solution')
+        .where('ticket_id', id);
+}
