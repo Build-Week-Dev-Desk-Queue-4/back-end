bf3683f72fb25557a20787213fa50e4ac500809b
Vincent Sanders <vincesanders@hotmail.com>
2020-03-03 14:50:40 -0600

    Added input validation to login to throw more explicit errors. Added validation to put endpoints on all routers to ensure data can only be edited by SLs, TLs and the user themselves.

--- utils/user-middleware/validateUserAction.js
+++ utils/user-middleware/validateUserAction.js
@@ -1,3 +1,5 @@
+//confirms through tokens that only team leads, section leads or the user themselves can do this action.
+
 module.exports = (req, res, next) => {
     if (req.decodedToken && req.decodedToken.role && (req.decodedToken.role.toLowerCase() === 'team lead' || req.decodedToken.role.toLowerCase() === 'section lead')) {
         next();
